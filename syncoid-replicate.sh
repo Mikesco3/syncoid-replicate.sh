c14828656b7588dab36e93665cf8378f49de009c
Mikesco3 <mikesco3@gmail.com>
2024-07-08 17:48:36 -0500

    Update syncoid-replicate.sh
    Added a 3rd argument for support to replicate to multiple targets

--- syncoid-replicate.sh
+++ syncoid-replicate.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-# Check if both source and destination are provided as arguments
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <source ZFS dataset> <destination ZFS dataset>"
+# Check if at least source and destination are provided as arguments
+if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
+  echo "Usage: $0 <source ZFS dataset> <destination ZFS dataset> [<identifier>]"
+  echo "Use a keyword for <identifier> as the 3rd argument when replicating to multiple targets."
   exit 1
 fi
 
 FROM="$1"
 TO="$2"
+IDENTIFIER=""
 
-# Defined a function to store the syncoid command and options
+# Check if the third argument (identifier) is provided
+if [ "$#" -eq 3 ]; then
+  IDENTIFIER="--identifier=$3"
+fi
+
+# Define a function to store the syncoid command and options
 SYNC_COMMAND() {
-  /usr/sbin/syncoid --force-delete --quiet --no-clone-handling --no-sync-snap  "$@"
+  /usr/sbin/syncoid --force-delete --quiet "$@" $IDENTIFIER
   ## If you want output, comment the previous line and uncomment the following one.
-  # /usr/sbin/syncoid --force-delete --no-clone-handling  "$@"
+  # /usr/sbin/syncoid --force-delete "$@" $IDENTIFIER
 }
 
 # Sync the ZFS datasets
@@ -21,7 +28,7 @@ SYNC_COMMAND "$FROM" "$TO"
 
 # Check if the syncoid command was successful
 if [ "$?" -eq 0 ]; then
- ## if you want output, uncomment the next two lines
+  ## If you want output, uncomment the next two lines
   echo "Sync completed successfully."
 else
   echo "Sync encountered an error."
